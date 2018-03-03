addc416fb800fe788ecaaaa442beb56cfce1446a
Justin Lin <caterpillar@openhome.cc>
2018-03-03 17:20:58 +0800

    before refactor

---
+++ js/tetrix_gui.js
@@ -0,0 +1,93 @@
+  function TetrixBox(boxWidthInBlk, boxHeightInBlk, 
+                   blockWidth,  blockHeight) {
+    boxWidthInBlk = boxWidthInBlk || 4;
+    boxHeightInBlk = boxHeightInBlk || 4;
+    blockWidth = blockWidth || 25;
+    blockHeight = blockHeight || 25;
+    
+    var isBlockBorder = false;
+    var tetrixPieces = [];
+    for(var i = 0; i < 7; i++) {
+        tetrixPieces[i] = new TetrixPiece(i + 1);
+    }
+    
+    var backgroundColor = new Color(255, 255, 255);
+    var width = boxWidthInBlk * blockWidth;
+    var height = boxHeightInBlk * blockHeight;
+    
+    var tetrixImages = null;
+    var currentPiece = currentImage = null;
+
+    this.setTetrixImages = function(images) {
+        tetrixImages = images;
+    };
+    
+    this.getTetrixImages = function() {
+        return tetrixImages;
+    };
+    
+    this.generate = function() {
+        var index = parseInt(Math.random() * 7);
+        currentPiece = tetrixPieces[index];
+        currentImage = tetrixImages[index];
+    };
+    
+    this.getTetrixPiece = function() {
+        return currentPiece;
+    };
+    
+    this.getTetrixImage = function() {
+        return currentImage;
+    };
+
+    this.setBackgroundColor = function(backgroundColor) {
+        backgroundColor = backgroundColor;
+    }
+    
+    this.getBackgroundColor = function() {
+        return backgroundColor;
+    };
+    
+    this.setBlockBorder = function(flag) {
+        isBlockBorder = flag;
+    };
+    
+    this.isBlockBorder = function() {
+        return isBlockBorder;
+    };
+    
+    this.getBlockWidth = function() {
+        return blockWidth;
+    };
+    
+    this.getBlockHeight = function() {
+        return blockHeight;
+    };
+    
+    this.getWidth = function() {
+        return width;
+    };
+    
+    this.getHeight = function() {
+        return height;
+    };
+    
+    function drawBlock(context, x, y) {
+        context.drawImage(currentImage, x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+        if(isBlockBorder) {
+            context.beginPath();
+            context.strokeStyle = 'rgb(150, 150, 150)';
+            context.rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+            context.stroke();
+        }
+    }
+
+    this.paint = function(context) {
+        // draw box background
+        context.fillStyle = backgroundColor.toString();
+        context.fillRect(0, 0, width, height);
+        for(var i = 0; i < 4; i++) {
+            drawBlock(context, currentPiece.getBlockXCoord(i) + 1, currentPiece.getBlockYCoord(i) + 1);
+        }
+    }
+}
---
+++ js/tetrix_model.js
@@ -0,0 +1,160 @@
+function TetrixPiece(type) {
+    type = type || parseInt(Math.random() * 7 + 1);
+    
+    var pieceType = 0;
+    var coordinates = [];
+    for(var i = 0; i < 4; i++) {
+        coordinates[i] = [];
+    }
+    
+    this.initialize = function(type) {
+        if (type < 1 || type > 7) {
+            type = 1;
+        }
+        // set coord. for each block
+        pieceType = type;
+        for(var i = 0 ;i < 4 ;i++) {
+            coordinates[i][0] = TetrixPiece.pieceTypes[pieceType - 1][i][0];
+            coordinates[i][1] = TetrixPiece.pieceTypes[pieceType - 1][i][1];
+        }
+    };
+    
+    this.initialize(type);
+
+    this.rotateLeft = function() {
+        // do not rotate square piece
+        if(pieceType == 5) {
+            return;
+        }
+        var tmp;
+        for(var i = 0; i < 4; i++) {
+            tmp = this.getBlockXCoord(i);  
+            this.setBlockXCoord(i, this.getBlockYCoord(i));
+            this.setBlockYCoord(i, -tmp);
+        }
+    };
+
+    this.rotateRight = function() {
+        // do not rotate square piece
+        if(pieceType == 5) {
+            return;
+        }
+        var tmp;
+        for(var i = 0; i < 4; i++) {
+            tmp = this.getBlockXCoord(i);
+            this.setBlockXCoord(i, -this.getBlockYCoord(i));
+            this.setBlockYCoord(i, tmp);
+        }
+    };
+    
+    // get the type of piece
+    this.getType = function() {
+        return pieceType;
+    };
+    
+    this.getBlockXCoord = function(index) {
+         return coordinates[index][0];
+    };
+  
+    this.getBlockYCoord = function(index) {
+        return coordinates[index][1];
+    };
+
+    this.getMinX = function() {
+        var tmp = coordinates[0][0];
+        for(var i = 1 ; i < 4 ; i++) {
+            if(tmp > coordinates[i][0]) {
+                tmp = coordinates[i][0];
+            }
+        }
+        return tmp;
+    };
+
+    this.getMaxX = function() {
+        var tmp = coordinates[0][0];
+        for(var i = 1 ; i < 4 ; i++) {
+            if(tmp < coordinates[i][0]) {
+                tmp = coordinates[i][0];
+            }
+        }
+        return tmp;
+    };
+
+    this.getMinY = function() {
+        var tmp = coordinates[0][1];
+        for(var i = 1; i < 4; i++) {
+            if(tmp > coordinates[i][1]) {
+                tmp = coordinates[i][1];
+            }
+        }
+        return tmp;
+    };
+
+    this.getMaxY = function() {
+        var tmp = coordinates[0][1];
+        for(var i = 1; i < 4; i++) {
+            if (tmp < coordinates[i][1]) {
+                tmp = coordinates[i][1];
+            }
+        }
+        return tmp;
+    };
+
+    this.setBlockXCoord = function(index, value) {
+        coordinates[index][0] = value;
+    };
+
+    this.setBlockYCoord = function(index, value) {
+        coordinates[index][1] = value;
+    };
+
+    this.setBlockCoord = function(index, x, y) {
+        coordinates[index][0] = x;
+        coordinates[index][1] = y;
+    };
+}
+
+// seven types of pieces
+TetrixPiece.pieceTypes = [[[ 0,-1],
+                           [ 0, 0],
+                           [-1, 0],
+                           [-1, 1]],
+                             
+                          [[ 0,-1],
+                           [ 0, 0],
+                           [ 1, 0],
+                           [ 1, 1]],
+
+                          [[ 0,-1],
+                           [ 0, 0],
+                           [ 0, 1],
+                           [ 0, 2]],
+
+                           [[-1, 0],
+                            [ 0, 0],
+                            [ 1, 0],
+                            [ 0, 1]],
+
+                           [[ 0, 0],
+                            [ 1, 0],
+                            [ 0, 1],
+                            [ 1, 1]],
+
+                           [[-1,-1],
+                            [ 0,-1],
+                            [ 0, 0],
+                            [ 0, 1]],
+
+                           [[ 1,-1],
+                            [ 0,-1],
+                            [ 0, 0],
+                            [ 0, 1]]];
+                            
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.toString = function() {
+        return 'rgb(' + [this.r, this.g, this.b].join() + ')';
+    };
+}                            
