1526fc8d622df57f0a8bed7cd5d39556fd72b95c
Justin Lin <caterpillar@openhome.cc>
2018-03-04 15:04:07 +0800

    clean code befor refactoring

--- js/tetrix_model.js
+++ js/tetrix_model.js
@@ -113,3 +113,233 @@ function randomFreezePiece() {
     let index = parseInt(Math.random() * 7);
     return pieces[index];
 }
+
+function TetrixGround(xblocks = 10, yblocks = 20) { 
+    let ground = Array(xblocks)
+                   .fill()
+                   .map(_ => Array(yblocks).fill(0));
+        
+    let xOffset = parseInt(xblocks / 2) - 1;
+    let yOffset = 1;
+    
+    let emptyLines = 0;
+    let removedLines = 0;
+    let score = 0;
+    
+    let isGameover = false;
+    let isOperable = true;
+
+    let tetrixPiece;
+    let testPiece;
+
+    this.isMovable = function(xStep, yStep) {
+        for(var i = 0; i < 4; i++) {
+            var x = tetrixPiece.xOfBlock(i) + xOffset + xStep;
+            var y = tetrixPiece.yOfBlock(i) + yOffset + yStep;
+
+            if(x < 0 || x >= xblocks || y >= yblocks) {
+                return false;
+            }
+
+            if(ground[x][y] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+    
+    this.addPieceOfType = function(pieceType) {
+        tetrixPiece = new TetrixPiece(pieceType);
+        testPiece = new TetrixPiece(pieceType);    
+        
+        xOffset = xblocks / 2 - 1;
+        if(tetrixPiece.minimumY < 0) {
+            yOffset = Math.abs(tetrixPiece.minimumY);
+        }
+        else {
+            yOffset = 0;
+        }
+        
+        for(var i = 0; i < 4; i++) {
+            var x = tetrixPiece.xOfBlock(i) + xOffset;
+            var y = tetrixPiece.yOfBlock(i) + yOffset;
+
+            if(ground[x][y] != 0) {
+                isGameover = true;
+            }
+        }
+    };
+    
+    this.reset = function() {
+        emptyLines = 0;
+        score = 0;
+        removedLines = 0;
+        isGameover = false; 
+        isOperable = true;
+        
+        xOffset = parseInt(xblocks / 2) - 1;
+        if(tetrixPiece.minimumY < 0) {
+            yOffset = Math.abs(tetrixPiece.minimumY);
+        }
+        else {
+            yOffset = 0;
+        }
+        
+        for(var i = 0; i < xblocks; i++) {
+            for(var j = 0; j < yblocks; j++) {
+                ground[i][j] = 0;
+            }
+        }
+    };
+    
+    this.moveTetrixLeft = function() {
+        if(this.isMovable(-1, 0)) {
+            xOffset--;
+        }
+    };
+
+    this.moveTetrixRight = function() {
+        if(this.isMovable(1, 0)) {
+            xOffset++;
+        }
+    };
+
+    this.moveTetrixDown = function() {
+        if(this.isMovable(0, 1) && isOperable) { 
+            yOffset++; 
+        }
+    };
+
+    this.dropTetrix = function() {
+        while(this.isMovable(0, 1) && isOperable) {
+            yOffset++;
+        }
+    };
+
+    this.rotateTetrix = function(clockwise) {
+        for(var i = 0; i < 4; i++) {
+            testPiece.setBlockCoord(i, tetrixPiece.xOfBlock(i), tetrixPiece.yOfBlock(i));
+        }
+
+        if(clockwise) {
+            testPiece.rotateRight();
+        }
+        else {
+            testPiece.rotateLeft();
+        }
+
+        for(var i = 0; i < 4; i++) {
+            var x = testPiece.xOfBlock(i) + xOffset;
+            var y = testPiece.yOfBlock(i) + yOffset;
+
+            if(x < 0 || x >= xblocks || y >= yblocks || y < 0) {
+                return;
+            }
+
+            if(ground[x][y] != 0) {
+                return;
+            }
+        } 
+       
+        for(var i = 0; i < 4; i++) {
+            tetrixPiece.setBlockCoord(i, testPiece.xOfBlock(i), testPiece.yOfBlock(i));
+        }
+    };
+    
+    this.updateGround = function() {
+        isOperable = false;
+        
+        if(!isGameover && !this.isMovable(0, 1)) {
+            // touch the bottom of playground or the top of one piece 
+            for(var i = 0; i < 4; i++) {
+                var x = tetrixPiece.xOfBlock(i) + xOffset;
+                var y = tetrixPiece.yOfBlock(i) + yOffset;
+                ground[x][y] = tetrixPiece.type + 1;
+            }
+        
+            var pieceTop = tetrixPiece.minimumY + yOffset;
+            if(pieceTop < emptyLines) {
+                emptyLines = pieceTop;
+            }
+        
+            removeFullLines();
+        }
+        
+        isOperable = true;
+    };
+    
+    function removeFullLines() {
+        var count = 0;
+        for(var j = yblocks - 1; j > emptyLines; j--) {
+            var isFullLine = true;
+            for(var i = 0; i < xblocks; i++) {
+                if(ground[i][j] == 0) {
+                    isFullLine = false;
+                    break;
+                }
+            }
+
+            if(isFullLine) {
+                count++;
+                for(var k = j; k > emptyLines; k--) {
+                    for(var i = 0; i < xblocks; i++) {
+                        if(ground[i][k] != ground[i][k-1]) {
+                            ground[i][k] = ground[i][k-1];
+                        }
+                    }
+                }
+                
+                for(var i = 0; i < xblocks; i++) {
+                    ground[i][emptyLines] = 0;
+                }
+                emptyLines++;
+                j++;
+                
+                removedLines++;
+            }
+        }
+        
+        score = score + parseInt(Math.pow(2, count));
+    };
+    
+    this.getGroundHeightInBlk = function() {
+        return yblocks;
+    };
+    
+    this.getGroundWidthInBlk = function() {
+        return xblocks;
+    };
+    
+    this.getTetrixPiece = function() {
+        return tetrixPiece;
+    };
+    
+    this.setTetrixPiece = function(tetrixPiece) {
+        tetrixPiece = tetrixPiece;
+    };
+    
+    this.getGroundInfo = function() {
+        return ground;
+    };
+    
+    this.isGameover = function() {
+        return isGameover;
+    };
+    
+    this.getScore = function() {
+        return score;
+    };
+    
+    this.getRemovedLines = function() {
+        return removedLines;
+    };
+    
+    this.getXOffset = function() {
+        return xOffset;
+    };
+    
+    this.getYOffset = function() {
+        return yOffset;
+    };
+}
